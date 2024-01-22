980cda3f6c489d78043cd428d4f5c6d45a2310cd
Juan E. Sanchez <juan@tcad.com>
2024-01-22 17:39:38 -0600

    tetgen file writing support

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,3 +1,6 @@
+0.0.7
+Write tetgen
+
 0.0.6
 Support vector components
 
--- tdrconvert/tdr_convert.py
+++ tdrconvert/tdr_convert.py
@@ -23,11 +23,12 @@ def run():
     parser.add_argument('--load_datasets', help='write data sets', default=False, action='store_true')
     parser.add_argument('--tecplot',       help='the tecplot file to output', required=False)
     parser.add_argument('--devsim',        help='the devsim file to output', required=False)
-    parser.add_argument('--gmsh',        help='the gmsh file to output', required=False)
-    parser.add_argument('--gmsh_import',        help='the file to write the devsim commands to recreate a new gmsh device', required=False)
+    parser.add_argument('--gmsh',          help='the gmsh file to output', required=False)
+    parser.add_argument('--gmsh_import',   help='the file to write the devsim commands to recreate a new gmsh device', required=False)
     parser.add_argument('--device_name',   help='the device name', default="device", required=False)
-    parser.add_argument('--scale',   help='coordinate scaling factor', default=1, type=float, required=False)
+    parser.add_argument('--scale',         help='coordinate scaling factor', default=1, type=float, required=False)
     parser.add_argument('--drop_interfaces_at_contact', help="drop interfaces from nodes at contact", default=False, action='store_true')
+    parser.add_argument('--tetgen',        help='the base name for tetgen files to output', required=False)
 
     args = parser.parse_args()
 
@@ -39,11 +40,16 @@ def run():
         ds.write_devices(file=args.devsim)
     if args.tecplot:
         ds.write_devices(file=args.tecplot, type='tecplot')
-    if args.gmsh:
+
+    if args.gmsh or args.tetgen:
         all_info = write_gmsh.get_all_info(args.device_name)
+
+    if args.gmsh:
         write_gmsh.write_gmsh(filename=args.gmsh, all_info=all_info)
         if args.gmsh_import:
             write_gmsh.write_gmsh_import(args.gmsh, args.gmsh_import, all_info['device_info'])
+    if args.tetgen:
+        write_gmsh.write_tetgen(basename=args.tetgen, all_info=all_info)
 
 
 if __name__ == "__main__":
